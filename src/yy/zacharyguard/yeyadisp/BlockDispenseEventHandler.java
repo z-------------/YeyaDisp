c8ed45d5873ce5287ef5b83edd374a00d6b95e8a
Zachary Guard <zacharyguard@gmail.com>
2017-09-06 16:08:28 +0800

    It works!

---
+++ src/yy/zacharyguard/yeyadisp/BlockDispenseEventHandler.java
@@ -0,0 +1,34 @@
+package yy.zacharyguard.yeyadisp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Server;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
+
+public class BlockDispenseEventHandler implements Listener {
+	
+	Main plugin;
+	Server server = Bukkit.getServer();
+	
+	public BlockDispenseEventHandler(Main instance) {
+		plugin = instance;
+	}
+	
+	@EventHandler
+    public void onBlockDispense(BlockDispenseEvent event) {
+    	Block block = event.getBlock();
+    	if (block.getType() == Material.DISPENSER) {
+    		Container dispenser = (Container) block.getState();
+    		Location dispenserLocation = dispenser.getLocation();
+    		if (plugin.yDispLocations.contains(dispenserLocation)) {
+    			plugin.replenishDispenser(dispenserLocation);
+    		}
+    	}
+    }
+	
+}
--- src/yy/zacharyguard/yeyadisp/Main.java
+++ src/yy/zacharyguard/yeyadisp/Main.java
@@ -4,30 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.block.Container;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
 	Logger logger = getLogger();
+	Server server = getServer();
 	
-	List<Container> yeyaDisps = new ArrayList<Container>();
+	List<Location> yDispLocations = new ArrayList<Location>();
 	
 	@Override
     public void onEnable() {
 		logger.info("YeyaDisp ready.");
+		Bukkit.getPluginManager().registerEvents(new BlockDispenseEventHandler(this), this); // register EventHandlers
     }
    
     @Override
@@ -40,13 +41,18 @@ public class Main extends JavaPlugin {
             Command command,
             String label,
             String[] args) {
-        if (command.getName().equalsIgnoreCase("yeyadisp") && sender instanceof Player) {
+        if (
+        		(command.getName().equalsIgnoreCase("yeyadisp") || command.getName().equalsIgnoreCase("ydisp"))
+        		&& sender instanceof Player
+        		) {
         	LivingEntity player = (LivingEntity) sender;
         	Block targetBlock = player.getTargetBlock(null, 5);
         	if (targetBlock.getType() == Material.DISPENSER) {
         		player.sendMessage("Targeted block is dispenser. Proceeding.");
-        		Container dispenser = (Container) targetBlock;
-        		yeyaDisps.add(dispenser);
+        		Container dispenser = (Container) targetBlock.getState();
+        		Location dispenserLocation = dispenser.getLocation();
+        		yDispLocations.add(dispenserLocation);
+        		replenishDispenser(dispenserLocation);
         	} else {
         		player.sendMessage("Targeted block is not dispenser. Aborting.");
         	}
@@ -57,15 +63,15 @@ public class Main extends JavaPlugin {
         return false;
     }
     
-    @EventHandler
-    public void onBlockDispense(BlockDispenseEvent event) {
-    	Block block = event.getBlock();
-    	ItemStack itemDispensed = event.getItem();
-    	
-    	if (block.getType() == Material.DISPENSER) {
-    		Container dispenser = (Container) block;
-    		MetadataValue containedItems = dispenser.getMetadata("Items").get(0);
-    		// TODO: loop through contained items and set each count to 64, then re-add the itemDispensed if there isn't already an ItemStack with its type and data.
-    	}
+    public boolean replenishDispenser(Location location) {
+    	Inventory inventory = ((Container) location.getBlock().getState()).getInventory();
+		for (int i = 0; i < 9; i++) {
+			ItemStack itemStack = inventory.getItem(i);
+			if (itemStack != null) {
+				itemStack.setAmount(itemStack.getMaxStackSize());
+				inventory.setItem(i, itemStack);
+			}
+		}
+		return true;
     }
 }
